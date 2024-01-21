8f791363ac03a5ab224b3404aa8d221c66a7e2d8
Mateus Dias <mateuussilvapb@gmail.com>
2024-01-21 12:45:35 -0300

    Feature: Criação do projeto

---
+++ src/main/java/io/github/mateuussilvapb/reference/ReferenceApplication.java
@@ -0,0 +1,13 @@
+package io.github.mateuussilvapb.reference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mateuussilvapb/reference/ReferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mateuussilvapb.reference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
